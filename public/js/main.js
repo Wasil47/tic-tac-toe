6c035460c5eae6fcde1b792f307f1dceaada7c7b
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-03-04 17:59:01 +0100

    add points, shake anim. Looks good. v1.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
-const ttt = document.getElementById('tic-tac-toe');
-
-// Create in selected elements (# or .) n-number of divs
+// Create in selected elements n-number of divs
 const boxes = [
   ['#tic-tac-toe', 9]
 ];
@@ -33,19 +31,34 @@ const winCombos = [
 
 let playerPicks = [];
 let AIpicks = [];
+let playerPoints = 0;
+let AIpoints = 0;
 
+// creat arr with positions (possible to pick)
 let arrPosition = [];
 for (let i=0; i<9;i++){ arrPosition.push(i)};
 
-const startOver = ()=>{
-  playerPicks = [];
-  AIpicks = [];
-  arrPosition = [];
+const clear = ()=> {
   for (let i=0; i<9;i++){ arrPosition.push(i)}; // fill arrPosition (0-8)
   for (const pos of arrDiv) {
     pos.classList.remove('x');
     pos.classList.remove('o');
     pos.innerHTML = '';
+    pos.classList.remove('shake');
+  }
+};
+// Reset game
+const startOver = ()=>{
+  playerPicks = [];
+  AIpicks = [];
+  arrPosition = [];
+  
+  setTimeout(clear, 2000);
+};
+// add class/animation to position which won
+const showCombo = (el) => {
+  for (const e of el) {
+    arrDiv[e].classList.add('shake');
   }
 };
 
@@ -56,52 +69,56 @@ const checkWin = (picks)=>{
     for (let i=0; i<picks.length; i++) {
       if (combo.includes(picks[i])){
         points++;          
-        if (points === 3) {
-          console.log(picks+' won!');
-          setTimeout(startOver, 1000);           
+        if (points === 3) { // if win
+          startOver();
+          showCombo(combo);
+          return true;
         }                  
       }
     } 
   }
 };
 
+// Do some magic when you click
 const playerClick = (e) => {
   e.addEventListener('click', ()=>{
-    if (e.classList.length <= 1) { // do not let pick picked item
+    if (e.classList.length <= 1 && arrPosition.length>=1) { // do not let pick alraedy picked item
       e.classList.add('x');
       e.innerHTML = 'X';
-      playerPicks.push(parseInt(e.classList[0]));
+      playerPicks.push(parseInt(e.classList[0]));      
       
-      const pRemove = arrPosition.indexOf(parseInt(e.classList[0]));
       // remove picked int from arrPosition
+      const pRemove = arrPosition.indexOf(parseInt(e.classList[0]));
       if (pRemove != -1) {
         arrPosition.splice(pRemove, 1);
       } else {
-        console.log('Do NOT pick same position!');
+        console.log('Wait!');
       };
-      console.log(playerPicks);
-      
-      // check if win combo includes player picks
-      checkWin(playerPicks);
 
-      // AI pick pos and remove int from arrPosition
-      if (arrPosition.length >= 1) {
-        const randomAIpick = Math.floor(Math.random()*(arrPosition.length));
-        arrDiv[arrPosition[randomAIpick]].classList.add('o');
-        arrDiv[arrPosition[randomAIpick]].innerHTML = 'O';
-        AIpicks.push(parseInt(arrPosition[randomAIpick]));
-        console.log(AIpicks);        
-
-        arrPosition.splice(randomAIpick, 1);
+      if (checkWin(playerPicks)) {
+        console.log('Player Win!');
+        playerPoints++
       } else {
-        setTimeout(startOver, 1000);
+        // AI pick pos (random) and remove int from arrPosition
+        if (arrPosition.length >= 1) {
+          const randomAIpick = Math.floor(Math.random()*(arrPosition.length));
+          arrDiv[arrPosition[randomAIpick]].classList.add('o');
+          arrDiv[arrPosition[randomAIpick]].innerHTML = 'O';
+          AIpicks.push(parseInt(arrPosition[randomAIpick]));
+          arrPosition.splice(randomAIpick, 1);
+        } else {
+          startOver();
+        };
+        if (checkWin(AIpicks)) {
+          console.log('AI Win!');
+          AIpoints++        
+        };
       }
-      checkWin(AIpicks);
-
+      document.querySelector('.AIpoints').innerHTML = 'AI: '+AIpoints;
+      document.querySelector('.playerPoints').innerHTML = 'You: '+playerPoints;
     } else {
-      console.log('Do NOT pick same position'); 
-    }
-
+      console.log('Do NOT pick same position or WAIT!'); 
+    };
   });
 };
 
